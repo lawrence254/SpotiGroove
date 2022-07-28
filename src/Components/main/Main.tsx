500303139bb23d9ddef391a7d178d2669a90f434
lawrence254 <lawrenslava1@gmail.com>
2022-07-28 22:31:37 +0300

    Add the song Item component. Allow users to pick songs

--- src/Components/main/Main.tsx
+++ src/Components/main/Main.tsx
@@ -1,14 +1,17 @@
-import tracksInfo from '../../assets/player/tracksInfo';
+import {Music} from '../../assets/player/tracksInfo';
 import Banner from './Banner/Banner';
 import NavBar from './MainNav/NavBar';
 import Player from './Player/Player';
+import SongItem from './Songs/SongItem';
 import './styles.scss';
 const Main = () => {
+    
     return (
         <div className="container">
             <NavBar />
             <Banner />
-            <Player {...tracksInfo} />
+            <SongItem/>
+            <Player {...Music} />
         </div>
     )
 }
